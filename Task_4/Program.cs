66ca8415d53d1c3461edfcde7850a3fda243b86a
Xenia <listk98@yandex.ru>
2022-04-12 01:06:30 +0300

    Add Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,40 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+int[,,] RandomArray (int row, int column,int z)
+{
+    int[,,] array = new int[row,column,z];
+    int val = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                array[i,j,k] = val++;
+            }
+        }
+    }
+    
+    return array;
+}
+
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                Console.Write($"{array[i,j,k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] array = RandomArray(4,4,4);
+Console.WriteLine();	
+PrintArray(array);
